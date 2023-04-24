813b34121bbf02f3003b25a2f59cc51780bbbed4
Mitchell So <110700396+Miso98@users.noreply.github.com>
2023-04-24 10:23:36 -0700

    Delete pset1q1p1.m

--- vectorcal/pset1q1p1.m
+++
@@ -1,9 +0,0 @@
-t = linspace ( 0, 4 * pi, 100); 
-x= (t - sin (t) );
-y= (1 - cos (t)); 
-plot (x,y)
-%plot 
-xlabel ( 'x')
-ylabel ('y')
-title ( 'motion of point on the circumference of a circle')
-grid on
